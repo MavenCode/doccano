4ddcb3595f1a70e364031866c20b7b0cf5d3b442
charles adetiloye <charles@mavencode.com>
2020-11-21 17:07:21 -0600

    connection to the db fix

--- tools/run.sh
+++ tools/run.sh
@@ -2,16 +2,16 @@
 
 set -o errexit
 
-#echo "Database connection"
-#export DB_USER="admin"
-#export DB_PASSWORD="admin"
-#export DB_NAME DB_NAME="doccano-db"
-#export PSQL_CLOUD_INSTANCE="mlops-kubeflow-00:us-central2:sql-mlops-kubeflow-00-4a32-master"
-#
-#export GOOGLE_APPLICATION_CREDENTIALS="./doccano-ai.key.json"
-#chmod +x /doccano/cloud_sql_proxy
-#mkdir /doccano/cloudsql_dir
-#./cloud_sql_proxy -dir=/doccano/cloud_sql_proxy --instances=$PSQL_CLOUD_INSTANCE=tcp:5432 --credential_file=$GOOGLE_APPLICATION_CREDENTIALS &
+echo "Database connection"
+export DB_USER="admin"
+export DB_PASSWORD="admin"
+export DB_NAME DB_NAME="doccano-db"
+export PSQL_CLOUD_INSTANCE="mlops-kubeflow-00:us-central2:sql-mlops-kubeflow-00-4a32-master"
+
+export GOOGLE_APPLICATION_CREDENTIALS="./doccano-ai.key.json"
+chmod +x /doccano/cloud_sql_proxy
+mkdir /doccano/cloudsql_dir
+./cloud_sql_proxy -dir=/doccano/cloud_sql_proxy --instances=$PSQL_CLOUD_INSTANCE=tcp:5432 --credential_file=$GOOGLE_APPLICATION_CREDENTIALS &
 
 #export ADMIN_USERNAME="mavencode"
 #export ADMIN_PASSWORD="ap"
